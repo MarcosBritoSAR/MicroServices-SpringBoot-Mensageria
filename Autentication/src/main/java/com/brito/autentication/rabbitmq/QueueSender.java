c3e9db4e4491b698169f14ce74697c5a6fc3e5e7
Marcos Brito <marcosbryto92@gmail.com>
2025-01-16 00:13:07 -0300

    Fix integration issues

--- Autentication/src/main/java/com/brito/autentication/rabbitmq/QueueSender.java
+++ Autentication/src/main/java/com/brito/autentication/rabbitmq/QueueSender.java
@@ -1,19 +1,28 @@
 package com.brito.autentication.rabbitmq;
 
-import org.springframework.amqp.core.Queue;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class QueueSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
-    private Queue queue;
+    private Environment environment;
 
-    public void send(String message) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), message);
+    public boolean send(String message) {
+        try {
+            log.info(String.format("Enviando mensagem. Conteudo: %s. Fila: %s", message, environment.getProperty("RABBITMQ_NAME")));
+            rabbitTemplate.convertAndSend(System.getenv(environment.getProperty("RABBITMQ_NAME")), message);
+            return true;
+
+        }catch (Exception e){
+            return false;
+        }
     }
 }
