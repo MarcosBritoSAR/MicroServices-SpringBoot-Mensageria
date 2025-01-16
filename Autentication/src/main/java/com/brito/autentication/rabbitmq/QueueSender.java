f0694411e950c1dd50059e2c9a70e27d802e5846
Marcos Brito <marcosbryto92@gmail.com>
2025-01-16 00:27:23 -0300

    Correction Pull Request

--- Autentication/src/main/java/com/brito/autentication/rabbitmq/QueueSender.java
+++ Autentication/src/main/java/com/brito/autentication/rabbitmq/QueueSender.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 @Slf4j
 public class QueueSender {
@@ -18,10 +20,11 @@ public class QueueSender {
     public boolean send(String message) {
         try {
             log.info(String.format("Enviando mensagem. Conteudo: %s. Fila: %s", message, environment.getProperty("RABBITMQ_NAME")));
-            rabbitTemplate.convertAndSend(System.getenv(environment.getProperty("RABBITMQ_NAME")), message);
+            rabbitTemplate.convertAndSend(Objects.requireNonNull(environment.getProperty("RABBITMQ_NAME")), message);
             return true;
 
         }catch (Exception e){
+            log.error("Erro ao enviar mensagem", e);
             return false;
         }
     }
