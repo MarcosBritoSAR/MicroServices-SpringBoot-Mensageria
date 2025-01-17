b7ab8542f0f4a4f0868771d683d27bcacd49831a
Marcos Brito <marcosbryto92@gmail.com>
2025-01-17 12:45:55 -0300

    Adds pagination to the getAll endpoint with: 	•	Refactors UserMapper to include HATEOAS in the ResponseDto objects

--- Autentication/src/main/java/com/brito/autentication/web/controller/UserController.java
+++ Autentication/src/main/java/com/brito/autentication/web/controller/UserController.java
@@ -1,27 +1,20 @@
 package com.brito.autentication.web.controller;
 
-import java.util.List;
-
 import com.brito.autentication.entities.User;
 import com.brito.autentication.web.dto.CreateUserDto;
 import com.brito.autentication.web.dto.UpdateUserDto;
 import com.brito.autentication.web.dto.responses.UserResponseDtoDefault;
 import com.brito.autentication.web.dto.mapper.UserMapper;
+import com.brito.autentication.web.dto.responses.UserResponseDtoWithCpf;
 import com.brito.autentication.web.services.UserService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -54,9 +47,12 @@ public class  UserController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<UserResponseDtoDefault>> pegarTodosUsers() {
-        List<User> users = userService.buscarUsers();
-        return ResponseEntity.status(HttpStatus.OK).body(userMapper.toListDto(users));
+    public ResponseEntity<Page<UserResponseDtoDefault>> pegarTodosUsers(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", defaultValue = "12") Integer limit
+    ) {
+        Page<User> users = userService.buscarUsers(page, limit);
+        return ResponseEntity.status(HttpStatus.OK).body(userMapper.toPageDto(users));
     }
     
     @PutMapping("/{id}")
--- Autentication/src/main/java/com/brito/autentication/web/services/UserService.java
+++ Autentication/src/main/java/com/brito/autentication/web/services/UserService.java
@@ -12,9 +12,13 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jmx.export.notification.NotificationPublisher;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -28,6 +32,7 @@ public class UserService {
     private final UserRepostory userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final RoleRepository roleRepositorio;
+
     @Transactional
     public User salvar(User user) {
 
@@ -62,8 +67,11 @@ public class UserService {
     }
 
     @Transactional
-    public List<User> buscarUsers() {
-        return userRepository.findAll();
+    public Page<User> buscarUsers(Integer page, Integer limit) {
+
+        Pageable pageable = PageRequest.of(page, limit);
+        return userRepository.findAll(pageable);
+
     }
 
     @Transactional
@@ -74,7 +82,7 @@ public class UserService {
 
         User user = userRepository.findById(id).orElse(null);
 
-        user.setUsername (updateUser.getUsername());
+        user.setUsername(updateUser.getUsername());
         user.setPassword(updateUser.getPassword());
         return userRepository.save(user);
 
@@ -83,7 +91,7 @@ public class UserService {
     @Transactional
     public User buscarUserPorUserName(String user) {
         return (User) userRepository.findByUsername(user).orElseThrow(
-                () -> new EntityNotFoundException(String.format("User %s not found" , user)));
+                () -> new EntityNotFoundException(String.format("User %s not found", user)));
     }
 
 
